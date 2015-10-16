71d6df125045b3a506d92b8083d6ce8a05a63275
Diego <diegofk26@gmail.com>
2015-10-16 00:26:44 -0300

    Agregado de un par de cambios

---
+++ InformesPerl/inicializarAmbiente.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+MAEDIR="/home/diego/Documents/TPSO/sisops_grupo11/InformesPerl/mae"
+
+PROCDIR="/home/diego/Documents/TPSO/sisops_grupo11/InformesPerl/sospe"
+
+export MAEDIR
+
+export PROCDIR
