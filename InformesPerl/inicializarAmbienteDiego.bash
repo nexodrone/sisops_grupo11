28ab69a4f2b43224ccd9c3c8c3a163f233771f73
nexodrone <nexodrone@gmail.com>
2015-10-18 10:36:35 -0300

    inicializar con reportes

--- InformesPerl/inicializarAmbienteDiego.bash
+++ InformesPerl/inicializarAmbienteDiego.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
+REPODIR="/home/diego/Documents/TPSO/sisops_grupo11/InformesPerl/reportes"
 
 MAEDIR="/home/diego/Documents/TPSO/sisops_grupo11/InformesPerl/mae"
 
 PROCDIR="/home/diego/Documents/TPSO/sisops_grupo11/InformesPerl/sospe"
 
 export MAEDIR
-
+export REPODIR
 export PROCDIR
---
+++ InformesPerl/inicializarAmbienteNik.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPODIR="/home/nexodrone/Desktop/sisops_grupo11/InformesPerl/reportes"
+
+MAEDIR="/home/nexodrone/Desktop/sisops_grupo11/InformesPerl/mae"
+
+PROCDIR="/home/nexodrone/Desktop/sisops_grupo11/InformesPerl/sospe"
+
+export MAEDIR
+export REPODIR
+export PROCDIR
