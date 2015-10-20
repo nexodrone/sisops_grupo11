fa37e04ee880ed39ee0217ae9ec0b9f6cc4c41f3
nexodrone <nexodrone@gmail.com>
2015-10-20 17:00:47 -0300

    AFRALIST.pl solo

--- :setup/bin/AFRALIST.pl
+++ :setup/bin/AFRALIST.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl -w
-use strict;
-use warnings;
+#use strict;
+#use warnings;
 use Fcntl qw(:flock);
 use feature qw/switch/; 
-use Switch;
+#use Switch;
+no warnings 'experimental::smartmatch';
+
 
 our %posCamposCentrales = (
 			'codigo' => 0,
@@ -12,6 +14,7 @@ our %posCamposCentrales = (
 
  our %posCamposAgentes = (
 			'id' =>0,
+                         'nombreCompleto'=>2,
 	                 'oficina'=>3,
 			 'email'=>4
 			     
@@ -28,6 +31,13 @@ our %posCamposCodigosArea=(
 			'area' => 0,
 
 );
+
+our %posCamposUmbrales= (
+		         'idUmbral' => 0,
+			
+
+);
+
 
 our %posCamposLlamadasSospechosa= (
 				   'idCentral' => 0,
@@ -52,7 +62,11 @@ our %paisesDesc;
 our %codigosAreaDesc;
 our %rankingOficinas;
 our %rankingCentrales;
-
+our %rankingAgentes;
+our %rankingUmbrales;
+our %rankingLlamadaSospechosa;
+our %lineaDestinoArea;
+our %lineaDestinoPais;
 ###### Se valida que el comando no este en ejecución#####
 open(my $script_fh, '<', $0)
    or die("No se pudo abrir el archivo: $!\n");
@@ -64,11 +78,11 @@ unless (flock($script_fh, LOCK_EX|LOCK_NB)) {
 
 ###### Se valida ejecucion de AFRAINIC######
 # Si no existen las variables de ambiente sabemos que Afranic no se ejecuto
-#if (!exists($ENV{"MAEDIR"}) && !exists($ENV{"PROCDIR"}) && !exists($ENV{"INFODIR"}) && !exists($ENV{"NOVEDIR"}))
-#{
-#	 print "Previamente debe inicializar el ambiente\n";
-#     exit(1);	  
-#} 
+if (!exists($ENV{"MAEDIR"}) && !exists($ENV{"PROCDIR"})  && !exists($ENV{"NOVEDIR"}))
+{
+	 print "Previamente debe inicializar el ambiente\n";
+     exit(1);	  
+} 
 
 
 ###### MENU ##############################
@@ -84,10 +98,10 @@ if ($num_parametros < 1) {
     exit;
 }else{
 	my $primero=$ARGV[0];
-	if($primero eq "-a") { mostrarAyuda(); }
-	elsif($primero eq "-c") { consultar();}
+	if($primero eq "-h") { mostrarAyuda(); }
+	elsif($primero eq "-r") { consultar();}
 	elsif($primero eq "-i") { informar(); }
-	elsif($primero eq "-e") { estadisticas();}
+	elsif($primero eq "-s") { estadisticas();}
 	else { 
 		print "Opción incorrecta.\n";
     		mostrarAyuda();
@@ -101,11 +115,11 @@ if ($num_parametros < 1) {
 sub mostrarAyuda {
 
 print "Ayuda: AFRALIST [opción] [argumentos] 
-	  -a                  			Muestra esta ayuda.
-	  -g                  			Grabar. Se utiliza como complemento de -c o -i. Ej: -c 'Resolucion' -g
-	  -c[palabra clave]   			Consulta por palabra clave.
+	  -h                  			Muestra esta ayuda.
+	  -w                  			Grabar. 
+	  -r[palabra clave]   			Consulta .
 	  -i[lista de archivos resultado]   	Información
-	  -e                  			Estadísticas\n";
+	  -s                  			Estadísticas\n";
 }
 
 sub consultar {
@@ -150,8 +164,8 @@ return 1;
 
 sub cargarMapaCentrales
 {
-	my $ruta ='./MAEDIR';
-	#my $ruta = $ENV{"GRUPO"}.'/'.$ENV{"MAEDIR"};
+	#my $ruta ='./MAEDIR';
+	my $ruta = $ENV{"MAEDIR"};
 	my $filename = $ruta.'/CdC.mae';
 	open(my $fh,"<$filename")|| die "NO SE PUEDE REALIZAR LA CONSULTA. No se encontro el archivo $filename \n";
 		#Leo cada linea 
@@ -170,22 +184,27 @@ sub cargarMapaCentrales
 
 sub cargarMapaAgentes
 {
-    my $ruta ='./MAEDIR';
-	#my $ruta = $ENV{"GRUPO"}.'/'.$ENV{"MAEDIR"};
+        #my $ruta ='./MAEDIR';
+	my $ruta = $ENV{"MAEDIR"};
+        #print "$ruta";
 	my $filename = $ruta.'/agentes.mae';
+
+       
+
 	open(my $fh,"<$filename")|| die "NO SE PUEDE REALIZAR LA CONSULTA. No se encontro el archivo $filename \n";
 		#Leo cada linea 
 		while (my $row = <$fh>) {
 			chomp $row;
 			my @data = split(";",$row);
-			my $id = $data[$posCamposAgentes{'id'}];
+			my $id = $data[$posCamposAgentes{'nombreCompleto'}];
 			my $oficina = $data[$posCamposAgentes{'oficina'}];
 			my $email=$data[$posCamposAgentes{'email'}];
 			
                         $agentesOficina{$id}=$oficina;
 			$agentesMail{$id}=$email;
 			$rankingOficinas{$oficina}=0;
-                        #print "$rankingOficinas{$oficina}, $oficina \n";
+			$rankingAgentes{$id}=0;
+                        #print "$agentesMail{$id}, $id \n";
 
 		}
 	close ($fh);
@@ -199,8 +218,8 @@ sub cargarMapaAgentes
 sub cargarCodigoPaises
 {
 
-   my $ruta ='./MAEDIR';
-	#my $ruta = $ENV{"GRUPO"}.'/'.$ENV{"MAEDIR"};
+   #my $ruta ='./MAEDIR';
+	my $ruta = $ENV{"MAEDIR"};
 	my $filename = $ruta.'/CdP.mae';
 	open(my $fh,"<$filename")|| die "NO SE PUEDE REALIZAR LA CONSULTA. No se encontro el archivo $filename \n";
 		#Leo cada linea 
@@ -223,8 +242,9 @@ sub cargarCodigoPaises
 sub cargarCodigoArea
 {
 
-   my $ruta ='./MAEDIR';
-	#my $ruta = $ENV{"GRUPO"}.'/'.$ENV{"MAEDIR"};
+        #my $ruta ='./MAEDIR';
+	my $ruta =$ENV{"MAEDIR"};
+        
 	my $filename = $ruta.'/CdA.mae';
 	open(my $fh,"<$filename")|| die "NO SE PUEDE REALIZAR LA CONSULTA. No se encontro el archivo $filename \n";
 		#Leo cada linea 
@@ -232,10 +252,10 @@ sub cargarCodigoArea
 			chomp $row;
 			my @data = split(";",$row);
 			my $codigoArea = $data[$posCamposCodigosArea{'codigoArea'}];
-			my $nombrePais = $data[$posCamposCodigosArea{'area'}];
+			my $nombreArea = $data[$posCamposCodigosArea{'area'}];
 			
 			
-                        $codigosAreaDesc{$codigoArea}=$nombrePais;
+                        $codigosAreaDesc{$codigoArea}=$nombreArea;
 			
 
 		}
@@ -244,7 +264,30 @@ sub cargarCodigoArea
                        
 }
 
+sub cargarUmbrales
+{
+    #my $ruta ='./MAEDIR';
+	my $ruta =$ENV{"MAEDIR"};
+        
+	my $filename = $ruta.'/umbral.tab';
+	open(my $fh,"<$filename")|| die "NO SE PUEDE REALIZAR LA CONSULTA. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+		while (my $row = <$fh>) {
+			chomp $row;
+			my @data = split(";",$row);
+			my $idUmbral = $data[$posCamposUmbrales{'idUmbral'}];
+			
+			
+			
+                        $rankingUmbrales{$idUmbral}=0;
+			#print "$rankingUmbrales{$idUmbral}, $idUmbral,\n";
 
+		}
+	close ($fh);
+
+
+
+}
 
 
 #******************** calculo estadísticas**************
@@ -253,8 +296,8 @@ sub leerPorCentrales
 {
 
         limpiarRankingCentrales();
-        my $dir= './PROCDIR';
-	#my $dir = $ENV{"GRUPO"}.'/'.$ENV{"PROCDIR"};
+        #my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
 	
 
 	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
@@ -293,12 +336,221 @@ sub leerPorCentrales
        
 }
 
+sub leerPorUmbrales
+{
+
+limpiarRankingUmbrales();
+        #my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
+	
+
+	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
+        my @nodos = grep(!/^\./, (sort(readdir(DIR)))); # esquiva archivos ocultos, . y ..
+        closedir(DIR); 
+	
+	
+
+	foreach my $nodo (@nodos)  
+	{
+		  
+		 my $nombreArchivo = $nodo;
+                 
+                my $filename = $dir.'/'.$nombreArchivo;
+		open(ENT,"<$filename")|| die "NO SE PUEDE REALIZAR LA ACCIÓN. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+          	
+          	while( my $row = <ENT> ) { 
+                        #print "$row \n";
+			chomp($row);
+			my @data = split(";",$row);
+			my $idUmbral = $data[$posCamposLlamadasSospechosa{'idUmbral'}];
+
+                        
+
+                	$rankingUmbrales{$idUmbral}=$rankingUmbrales{$idUmbral}+1;
+
+                         #print "$rankingUmbrales{$idUmbral}, ' ',$idUmbral \n";
+		}
+                close(ENT);
+		
+	  }
+          #print "cantidad de registros $count \n";
+	
+
+
+
+
+}
+
+sub leerPorDestinoSospechoso
+{
+
+        limpiarRankingDestinoSospechoso();
+        #my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
+	
+
+	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
+        my @nodos = grep(!/^\./, (sort(readdir(DIR)))); # esquiva archivos ocultos, . y ..
+        closedir(DIR); 
+	
+	
+
+	foreach my $nodo (@nodos)  
+	{
+		  
+		 my $nombreArchivo = $nodo;
+                 
+                my $filename = $dir.'/'.$nombreArchivo;
+		open(ENT,"<$filename")|| die "NO SE PUEDE REALIZAR LA ACCIÓN. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+          	
+          	while( my $row = <ENT> ) { 
+                        #print "$row \n";
+			chomp($row);
+			my @data = split(";",$row);
+			my $lineaDestino = $data[$posCamposLlamadasSospechosa{'nroBlinea'}];
+                        my $codigoArea=$data[$posCamposLlamadasSospechosa{'nroBcda'}];
+                        my $codigoPais=$data[$posCamposLlamadasSospechosa{'nroBcdp'}];
+                        
+
+                	$rankingLlamadaSospechosa{$lineaDestino}=$rankingLlamadaSospechosa{$lineaDestino}+1;
+                        $lineaDestinoArea{$lineaDestino}=$codigoArea;
+                        $lineaDestinoPais{$lineaDestino}=$codigoPais;
+                        if ($codigoPais eq ''){
+			$lineaDestinoPais{$lineaDestino}=54;
+			}
+
+                         #print "$rankingUmbrales{$idUmbral}, ' ',$idUmbral \n";
+		}
+                close(ENT);
+		
+	  }
+          #print "cantidad de registros $count \n";
+	
+
+
+
+
+}
+
+
 sub leerPorOficinas
 {
         limpiarRankingOficinas();
 	#limpiarRankingCentrales();
-        my $dir= './PROCDIR';
-	#my $dir = $ENV{"GRUPO"}.'/'.$ENV{"PROCDIR"};
+        #my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
+	
+
+	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
+        my @nodos = grep(!/^\./, (sort(readdir(DIR)))); # esquiva archivos ocultos, . y ..
+        closedir(DIR); 
+	
+
+	foreach my $nodo (@nodos)  
+	{
+		  
+		 my $nombreArchivo = $nodo;
+                 
+                my $filename = $dir.'/'.$nombreArchivo;
+		open(ENT,"<$filename")|| die "NO SE PUEDE REALIZAR LA ACCIÓN. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+          	
+               
+		my @fields = split /_/, $nombreArchivo;
+                
+		my $oficina = $fields[0];
+                
+		$rankingOficinas{$oficina}=$rankingOficinas{$oficina} +1;
+
+		#print "$oficina,$rankingOficinas{$oficina}\n";
+
+
+          	#print "$nombreArchivo \n";
+
+
+                close(ENT);
+		
+	  }
+
+
+
+
+}
+
+
+#carga el ranking de agentes, previamente tiene que estar inicializado en 0
+sub leerPorAgentes
+{
+
+        limpiarRankingAgentes();
+
+        
+        #my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
+	
+
+	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
+        my @nodos = grep(!/^\./, (sort(readdir(DIR)))); # esquiva archivos ocultos, . y ..
+        closedir(DIR); 
+	
+	
+
+	foreach my $nodo (@nodos)  
+	{
+		  
+		 my $nombreArchivo = $nodo;
+                 
+                my $filename = $dir.'/'.$nombreArchivo;
+		open(ENT,"<$filename")|| die "NO SE PUEDE REALIZAR LA ACCIÓN. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+          	
+          	while( my $row = <ENT> ) { 
+                        #print "$row \n";
+			chomp($row);
+			my @data = split(";",$row);
+			#my $idCentral = $data[$posCamposLlamadasSospechosa{'idCentral'}];
+
+                        my $idAgente=$data[$posCamposLlamadasSospechosa{'idAgente'}];
+                       # print "$rankingAgentes{$idAgente},$idAgente \n";
+
+                	$rankingAgentes{$idAgente}=$rankingAgentes{$idAgente}+1;
+
+                         #print "$rankingAgentes{$idAgente}, ' ',$idAgente \n";
+		}
+                close(ENT);
+		
+	  }
+          
+	
+          
+       
+
+
+
+
+
+}
+
+sub limpiarRankingCentrales{
+           
+          foreach my $name (keys %rankingCentrales) {
+	    	 $rankingCentrales{$name}=0;
+	}
+		 
+                 
+
+             
+}
+
+sub limpiarRankingOficinas
+{
+
+     
+
+#my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
 	
 
 	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
@@ -318,7 +570,8 @@ sub leerPorOficinas
                
 		my @fields = split /_/, $nombreArchivo;
 		my $oficina = $fields[0];
-		$rankingOficinas{$oficina}=$rankingOficinas{$oficina} +1;
+                
+		$rankingOficinas{$oficina}=0;
 
 		#print "$oficina,$rankingOficinas{$oficina}\n";
 
@@ -330,33 +583,118 @@ sub leerPorOficinas
 		
 	  }
 
-
+      
 
 
 }
 
 
-sub limpiarRankingCentrales{
-           
-          foreach my $name (keys %rankingCentrales) {
-	    	 $rankingCentrales{$name}=0;
-	}
-		 
-                 
-
-             
-}
-
-sub limpiarRankingOficinas
+sub limpiarRankingDestinoSospechoso
 {
-      foreach my $name (keys %rankingOficinas) {
-	    	 $rankingOficinas{$name}=0;
-	}
+
+
+        #my $dir= './PROCDIR';
+	my $dir = $ENV{"PROCDIR"};
+	
+
+	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
+        my @nodos = grep(!/^\./, (sort(readdir(DIR)))); # esquiva archivos ocultos, . y ..
+        closedir(DIR); 
+	
+	
+
+	foreach my $nodo (@nodos)  
+	{
+		  
+		 my $nombreArchivo = $nodo;
+                 
+                my $filename = $dir.'/'.$nombreArchivo;
+		open(ENT,"<$filename")|| die "NO SE PUEDE REALIZAR LA ACCIÓN. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+          	
+          	while( my $row = <ENT> ) { 
+                        #print "$row \n";
+			chomp($row);
+			my @data = split(";",$row);
+			my $lineaDestino = $data[$posCamposLlamadasSospechosa{'nroBlinea'}];
+                        
+                        
+
+                	$rankingLlamadaSospechosa{$lineaDestino}=0;
+
+                         #print "$rankingUmbrales{$idUmbral}, ' ',$idUmbral \n";
+		}
+                close(ENT);
+		
+	  }
+          #print "cantidad de registros $count \n";
+	
+
+
+
 
 
 
 }
 
+sub limpiarRankingAgentes
+{
+
+
+my $dir = $ENV{"PROCDIR"};
+	
+
+	opendir(DIR,$dir) || die "# buscador : no puedo abrir $dir\n";
+        my @nodos = grep(!/^\./, (sort(readdir(DIR)))); # esquiva archivos ocultos, . y ..
+        closedir(DIR); 
+	
+	
+
+	foreach my $nodo (@nodos)  
+	{
+		  
+		 my $nombreArchivo = $nodo;
+                 
+                my $filename = $dir.'/'.$nombreArchivo;
+		open(ENT,"<$filename")|| die "NO SE PUEDE REALIZAR LA ACCIÓN. No se encontro el archivo $filename \n";
+		#Leo cada linea 
+          	
+          	while( my $row = <ENT> ) { 
+                        #print "$row \n";
+			chomp($row);
+			my @data = split(";",$row);
+			#my $idCentral = $data[$posCamposLlamadasSospechosa{'idCentral'}];
+
+                        my $idAgente=$data[$posCamposLlamadasSospechosa{'idAgente'}];
+                       # print "$rankingAgentes{$idAgente},$idAgente \n";
+
+                	$rankingAgentes{$idAgente}=0;
+
+                         #print "$rankingAgentes{$idAgente}, ' ',$idAgente \n";
+		}
+                close(ENT);
+		
+	  }
+
+
+
+
+
+}
+
+
+sub limpiarRankingUmbrales
+{
+
+foreach my $name (keys %rankingUmbrales) {
+	    	 $rankingUmbrales{$name}=0;
+                # print "$rankingUmbrales{$name}, $name \n";
+	}
+	
+
+
+
+}
 #************impresión**************************
 
 
@@ -370,13 +708,15 @@ my @keys = sort {$rankingCentrales{$b}  <=> $rankingCentrales{$a} } keys %rankin
 
 foreach my $key ( @keys ) {
     
+         if ($rankingCentrales{$key} !=0){
           $resultadoArchivo .= "$rankingCentrales{$key}  $key  $centralesDesc{$key}\n";
+	 }
 }
 
 
            print $resultadoArchivo;
 	if($pidioGuardar == 1){
-		grabar($resultadoArchivo,"resultado");	
+		grabar($resultadoArchivo,"rankingCentrales");	
 	}
 
 }
@@ -388,14 +728,107 @@ sub imprimirRankingOficinas
 		my @keys = sort {$rankingOficinas{$b}  <=> $rankingOficinas{$a} } keys %rankingOficinas;
 
 		foreach my $key ( @keys ) {
+                   
+                   if ($rankingOficinas{$key} != 0)
+                      	{
+
     
           		$resultadoArchivo .= "$rankingOficinas{$key}  $key \n";
+			}
 		}
 
 
            print $resultadoArchivo;
 	if($pidioGuardar == 1){
-		grabar($resultadoArchivo,"resultado");	
+		grabar($resultadoArchivo,"rankingOficinas");	
+	}
+
+
+
+}
+
+sub imprimirRankingAgentes
+{
+
+
+		my $resultadoArchivo = ""; 
+		my @keys = sort {$rankingAgentes{$b}  <=> $rankingAgentes{$a} } keys %rankingAgentes;
+
+		foreach my $key ( @keys ) {
+                   
+                   if ($rankingAgentes{$key} != 0)
+                      	{
+
+    
+          		$resultadoArchivo .= "$rankingAgentes{$key} $key $agentesMail{$key} $agentesOficina{$key} \n";
+			}
+		}
+
+
+           print $resultadoArchivo;
+	if($pidioGuardar == 1){
+		grabar($resultadoArchivo,"rankingAgentes");	
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+sub imprimirRankingUmbrales 
+{
+
+
+   my $resultadoArchivo = ""; 
+		my @keys = sort {$rankingUmbrales{$b}  <=> $rankingUmbrales{$a} } keys %rankingUmbrales;
+
+		foreach my $key ( @keys ) {
+                   
+                   if ($rankingUmbrales{$key} >1)
+                      	{
+
+    
+          		$resultadoArchivo .= "ocurrencia: $rankingUmbrales{$key} id de Umbral:$key  \n";
+			}
+		}
+
+
+           print $resultadoArchivo;
+	if($pidioGuardar == 1){
+		grabar($resultadoArchivo,"rankingUmbrales");	
+	}
+
+
+
+}
+
+sub imprimirRankingDestinoSospechoso
+{
+
+		my $resultadoArchivo = ""; 
+		my @keys = sort {$rankingLlamadaSospechosa{$b}  <=> $rankingLlamadaSospechosa{$a} } keys %rankingLlamadaSospechosa;
+
+		foreach my $key ( @keys ) {
+                   
+                   if ($rankingLlamadaSospechosa{$key} >0)
+                      	{
+                              
+
+    
+          $resultadoArchivo .= "ocurrencia: $rankingLlamadaSospechosa{$key}  codigo de pais:$lineaDestinoPais{$key} pais:$paisesDesc{$lineaDestinoPais{$key}} codigo de area:$lineaDestinoArea{$key} area:$codigosAreaDesc{$lineaDestinoArea{$key}}  numero de linea:$key  \n";
+			}
+		}
+
+
+           print $resultadoArchivo;
+	if($pidioGuardar == 1){
+		grabar($resultadoArchivo,"rankingLlamadaSospechosa");	
 	}
 
 
@@ -406,9 +839,9 @@ sub grabar
 {
 	my $resultado = $_[0];
 	my $nombre = $_[1];
+	my $ruta = $ENV{"GRUPO"}.'/'.'reportes';
 	
-	#my $ruta = $ENV{"GRUPO"}.'/'.$ENV{"REPODIR"};
-	my $ruta = "./REPODIR";
+	
 	my $epoc = time();
 	my $nombreArchivo = $ruta."/".$nombre."_".$epoc.".txt";
 	
@@ -431,12 +864,17 @@ sub clear_screen
 #*****************Estadisticas*********************
 sub menu_estadistica
 { 
+limpiarRankingOficinas();
 cargarMapaAgentes();
 cargarMapaCentrales();
 cargarCodigoPaises();
 cargarCodigoArea();
+cargarUmbrales();
 leerPorCentrales();
 leerPorOficinas();
+leerPorAgentes();
+leerPorUmbrales();
+leerPorDestinoSospechoso();
 #clear_screen();
 
 	my $input = '';
@@ -457,28 +895,29 @@ leerPorOficinas();
 	    $input = <STDIN>;
 	    chomp($input);
 
-	    switch ($input)
+	    given ($input)
 	    {
-		case '1'
+		when('1')
 		{
 		    imprimirRankingCentrales();
 
 		}
-		case '2'
-		{
+		when('2')
+
+		{   
                     imprimirRankingOficinas();
 		    	
 		}
-		case '3'{}
+		when('3') { imprimirRankingAgentes(); }
 		
-                case '4'{}
+                when('4'){imprimirRankingDestinoSospechoso();}
                 
-                case '5'{}
+                when('5') {imprimirRankingUmbrales(); }
                
-                case '6'{}
+                when('6') {}
 
 
-		else{
+		default {
 			print "Opción Incorrecta! \n";
 		}
 
